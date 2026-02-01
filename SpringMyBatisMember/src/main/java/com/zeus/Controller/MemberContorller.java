2efd24cc1d075405531fe431c0ba135ba30e241a
ldh6417 <ldh85316417@gmail.com>
2026-02-01 19:02:06 +0900

    [생성]  컨트롤러  회원정보 생성 및  jsp 생성

--- SpringMyBatisMember/src/main/java/com/zeus/Controller/MemberContorller.java
+++ SpringMyBatisMember/src/main/java/com/zeus/Controller/MemberContorller.java
@@ -1,19 +1,61 @@
 package com.zeus.Controller;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zeus.domain.Member;
 import com.zeus.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@MapperScan(basePackages = "com.zeus.mapper")
 @RequestMapping("/Member")
 public class MemberContorller {
-	@Autowired MemberService memberservice;
-	
+	@Autowired
+	private MemberService memberservice;
+
+	@GetMapping("/insertForm")
+	public String insert(Member member, Model molde) {
+		return "insertForm";
+	}
+
+	@PostMapping("/insert")
+	public String memberinsert(Member member, Model model) {
+		log.info("insert member =" + member.toString());
+		try {
+			int count = memberservice.register(member);
+			if (count > 0) {
+				model.addAttribute("messge", "%s 등록 성공".formatted(member.getName()));
+				return  "member/success";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("messge", "%s 등록 실패".formatted(member.getName()));
+		return  "member/failed";
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SpringMyBatisMember/src/main/java/com/zeus/domain/Member.java
@@ -0,0 +1,14 @@
+package com.zeus.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private int no;
+	private String id;
+	private String name;
+	private String phone;
+	private int regdate;
+}
+
+
